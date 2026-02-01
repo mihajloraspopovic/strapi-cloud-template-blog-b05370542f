516a4536fe88385c4d8b5382e1b50301d96789c0
Mihajlo Raspopovic <raspopovicm3@gmail.com>
2026-02-01 15:08:26 +0100

    Plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = () => ({
+  localazy: {
+    enabled: true,
+    config: {
+      // Default settings based on the example you provided
+      populateDefaultDepth: 5,
+      populateMaxDepth: 10,
+      skipCreatorFields: false,
+      enableSocketIO: true,
+    },
+  },
+});
